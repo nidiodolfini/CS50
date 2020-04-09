37880a2fbb548913d9c1bf0a07e30b23dab0311b
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-04-09 13:18:03 -0300

    update runoff

--- runoff.py
+++ runoff.py
@@ -1,12 +1,31 @@
 import sys
 preferences = [[],[]]
+candidates = []
+candidate = {}
 
-def vote(voter): #, rank, name
-    for i in range(voter):
-        vote = input(f'Rank {i+1}: ')
+def vote(voter, candidates): #, rank, name
+    for j in range(voter):
+        i = 1
+        while i < len(candidates)+1:
+            voted = input(f'Rank {i}: ')
+            for ind, vote in enumerate(candidates):
+                if voted in candidates[ind]['name']:
+                    candidates[ind]['vote'] += 1
+                    i += 1
+                    invalid = False
+                    break
+            else:
+                invalid = True
+            if invalid:
+                print('Invalid vote.')
+        print()
 
-
-
+def add_candidate(args):
+    for i in range(1, len(arg)):
+        candidate['name'] = arg[i]
+        candidate['vote'] = 0
+        candidate['eliminated'] = False
+        candidates.append(candidate.copy())
 
 def tabulate():
     pass
@@ -28,13 +47,12 @@ def eliminate(min):
     pass
 
 
-candidates = []
-candidate = {}
-
 arg = sys.argv
 if len(arg) > 10:
     print('max of candidates is 9')
     exit()
+add_candidate(arg)
 
-voters = int(input('Number of voters:'))
-vote(voters)
+votes = int(input('Number of voters:'))
+vote(votes, candidates)
+print(candidates)
